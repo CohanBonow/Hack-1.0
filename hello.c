ecabdeb9ddd0ae2c044115c9aa5e6010c0ae151d
CohanBonow <cohan.bonow@huskers.unl.edu>
2018-08-23 08:27:05 -0500

    Program writes My name and Major

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Cohan Bonow Braden Hadwiger
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Cohan Bonow and I am a computer Science Major");
+
+  return 0;
+{
